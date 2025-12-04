64b5fd63a28246f8ffef5bb41fc52abe56baac2f
Menardo Madiumba <menardotera@gmail.com>
2025-12-04 15:36:22 +0100

    type,print, et declaration de variable

--- main.py
+++ main.py
@@ -12,4 +12,24 @@ Une variable dois avoir un :
                         toujour commencer par une lettre,
                         ne dois pas contenir des espaces
 
+Types de données standars : (Entier (int), Decimal (float), Chaine de caractere (str), Booleen (bool))
+
+Le fonction type, retourne le type d'une chaine
+
+En ce qui concerne le variable flotant, decimal (nous devons toujour le separere avec un .)
+
+=> Pour afficher une variable ou va utiliser la fonction (print), 
+=> Pour verifier le type d'une variable on va utiliser la methode (type)
+
 """
+
+_age = 10
+_sexe = "M"
+decimal_variable = 10.25
+
+_bool_true = True
+_bool_false = False
+
+#print(type(_age), type(_sexe), type(decimal_variable))
+
+print(_bool_false)
