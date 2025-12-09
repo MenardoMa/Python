eeec73f4829e4427a3396f005d6d925c754e1c5b
Menardo Madiumba <menardotera@gmail.com>
2025-12-09 15:42:23 +0100

    Le base de la fonction input

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 #---------------------------------------------------------
 # Type de donnée [int, str, bool, float, liste, tuple, set]
 # La declaration de variable
+# Typage dynamique de donnée c.a.d on peut passé de string a integer
 #---------------------------------------------------------
-
-username = "moon"
-first_name = "chedo"
-
-resultat = username +" "+ first_name
-
-print(resultat)
