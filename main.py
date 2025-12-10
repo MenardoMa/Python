c13c4105680d3911afb7f39b5b140050a4a950e3
Menardo Madiumba <menardotera@gmail.com>
2025-12-10 11:34:49 +0100

    Fin Operateur

--- main.py
+++ main.py
@@ -1,46 +1,18 @@
-#
-# Les operateurs 
-# 
 
-"""
-Operateurs arithmetiques
 
-+, -, /, *, %, **
+# un dictionnaire c'est toujour cle valeur
+dico = {
+    'a': 10,
+    'b': 12
+}
 
-Les Operateurs d'affectation une affectation consiste a assigner une valeur a une variable
+print(dico)
+print(type(dico))
 
-=, +=, -=, *=, /=
+# un set c'est juste un objet avec de valeur
+print(type({1, 2, 3}))
 
-Les operateurs de comparaison
+# on peut definir aussi de priorité dans la logique en utilisant ()
+result = (10 + 5) * 9
 
-==, >, <, >=, <=, !=
-
-Les operateurs logique
-
-and, or, not
-
-identité 
-
-is Est-ce que ca vaut .... , is not Est-ce que ca va pas ....
-
-appartenance
-
-in, not int
-
-Les operateurs logique renvoie, True ou False par rapport a la condition
-Les operateurs d'affectation permet d'assigner une valeur dans une variable (=, += suivie de la valeur)
-Les operateurs logique retourn vraie ou fause par rapport a une condition (and == ET, &&, or == OU, ||)
-La concantenation consiste a mettre 2 chaines de caracteres cote a cote
-
-    [
-        Pour cela on va utiliser l'operateur +
-        Avec les chaine de caracteres on concataine mais avec le nombre on additionne
-    ]
-
-"""
-
-a = False 
-
-resultat = a is not True
-
-print(resultat)
+print(result)
