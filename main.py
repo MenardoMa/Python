4fba263874345b6d4516e1e0ec7299c7abdac1f2
Menardo Madiumba <menardotera@gmail.com>
2025-12-10 11:15:15 +0100

    Les condition et Les operateurs

--- main.py
+++ main.py
@@ -1,5 +1,46 @@
-#---------------------------------------------------------
-# Type de donnée [int, str, bool, float, liste, tuple, set]
-# La declaration de variable
-# Typage dynamique de donnée c.a.d on peut passé de string a integer
-#---------------------------------------------------------
+#
+# Les operateurs 
+# 
+
+"""
+Operateurs arithmetiques
+
++, -, /, *, %, **
+
+Les Operateurs d'affectation une affectation consiste a assigner une valeur a une variable
+
+=, +=, -=, *=, /=
+
+Les operateurs de comparaison
+
+==, >, <, >=, <=, !=
+
+Les operateurs logique
+
+and, or, not
+
+identité 
+
+is Est-ce que ca vaut .... , is not Est-ce que ca va pas ....
+
+appartenance
+
+in, not int
+
+Les operateurs logique renvoie, True ou False par rapport a la condition
+Les operateurs d'affectation permet d'assigner une valeur dans une variable (=, += suivie de la valeur)
+Les operateurs logique retourn vraie ou fause par rapport a une condition (and == ET, &&, or == OU, ||)
+La concantenation consiste a mettre 2 chaines de caracteres cote a cote
+
+    [
+        Pour cela on va utiliser l'operateur +
+        Avec les chaine de caracteres on concataine mais avec le nombre on additionne
+    ]
+
+"""
+
+a = False 
+
+resultat = a is not True
+
+print(resultat)
